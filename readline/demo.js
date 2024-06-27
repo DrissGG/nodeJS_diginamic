cb35b06ee6177f8b59f3ee1999c0d74ba8fb525e
Driss <drissgazzaoui1986@hotmail.com>
2024-06-27 15:07:29 +0200

    exo readline fait

--- readline/demo.js
+++ readline/demo.js
@@ -5,7 +5,20 @@ const r = readline.createInterface({
     output: process.stdout,
 })
 
-r.question("quel est votre nom ? ", (name) => {
-    console.log(`bonjour ${name} !`)
-    r.close()
-})
+// r.question("quel est votre nom ? ", (name) => {
+//     console.log(`bonjour ${name} !`)
+    
+// })
+
+function demandeText (){
+    r.question('Écrivez une ligne (ou tapez "exit" pour quitter) : ', (input) => {
+        if(input.toLowerCase() === "exit"){
+            console.log("Ciao")
+            r.close();
+        }else{
+            console.log(`Vous avez écrit : ${input}`);
+            demandeText();
+        }
+    })
+}
+demandeText()
